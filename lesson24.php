d66282f9fbc8531387c674a621c92e50cd39cb2e
Investor-v-stolovuy <koenigsegg970@mail.ru>
2022-09-06 15:44:02 +0400

    Двадцать четвертый урок

---
+++ lesson24.php
@@ -0,0 +1,11 @@
+<?php
+
+$eurosCount = 100;
+
+// BEGIN (write your solution here)
+$dollars_count = $eurosCount * 1.25;
+print_r($dollars_count);
+print_r("\n");
+$rubles_count = $dollars_count * 60;
+print_r($rubles_count);
+// END
