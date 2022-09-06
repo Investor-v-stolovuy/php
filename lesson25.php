28e7a4fa56a431062196c8f96931139431ec2b70
Investor-v-stolovuy <koenigsegg970@mail.ru>
2022-09-06 15:44:11 +0400

    Двадцать пятый урок

---
+++ lesson25.php
@@ -0,0 +1,14 @@
+<?php
+
+$info = "We couldn't verify you mother's maiden name.";
+$intro = "\nHere is important information about your account security.";
+
+$firstName = 'Joffrey';
+$greeting = 'Hello';
+
+// BEGIN (write your solution here)
+print_r($greeting . ", " . $firstName . "!");
+print_r($intro);
+print_r("\n");
+print_r($info);
+// END
