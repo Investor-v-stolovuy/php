566452b3098efa5008dfa0dea42e281dda59e583
Investor-v-stolovuy <koenigsegg970@mail.ru>
2022-09-07 14:31:36 +0400

    Двадцать седьмой урок

---
+++ lesson27.php
@@ -0,0 +1,10 @@
+<?php
+
+$king = 'King Balon the 6th';
+
+// BEGIN (write your solution here)
+$castleCount = 6;
+$roomsInEachCastle = 17;
+
+print_r($king . ' has ' . $castleCount * $roomsInEachCastle . ' rooms.');
+// END
